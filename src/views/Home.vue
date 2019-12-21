f90f0ab6172dfc073afbffb87199d218eebb15b0
guan xin <guanxin@tinytiger.cn>
2019-12-21 15:56:58 +0800

    加入内网链接

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,8 @@
           </template>
           <template slot="start">
             <b-navbar-item
-              :active="nav.is_active"
+              :active="nav.id === current_active_menu_id"
+              @click="handleChangeData(nav)"
               href="#"
               v-for="nav in navs"
               :key="nav.id"
@@ -91,6 +92,276 @@
 </template>
 
 <script>
+const navsData = [
+  {
+    id: 1,
+    title: "常用网站",
+    items: [
+      {
+        name: "百度",
+        href: "https://www.baidu.com",
+        color: "is-primary"
+      },
+      {
+        name: "谷歌",
+        href: "https://www.google.com",
+        color: "is-success"
+      },
+      {
+        name: "必应",
+        href: "https://cn.bing.com",
+        color: "is-danger"
+      },
+      {
+        name: "Stackoverflow",
+        href: "https://stackoverflow.com",
+        color: "is-warning"
+      },
+      {
+        name: "Segmentfault",
+        href: "https://segmentfault.com",
+        color: "is-info"
+      },
+      {
+        name: "腾讯企业邮",
+        href:
+          "https://exmail.qq.com/cgi-bin/loginpage?t=dm_loginpage&dmtype=bizmail",
+        color: "is-primary"
+      },
+      {
+        name: "谷歌邮箱",
+        href: "https://mail.google.com/mail",
+        color: "is-success"
+      },
+      {
+        name: "Outlook邮箱",
+        href: "https://outlook.live.com/mail",
+        color: "is-danger"
+      },
+      {
+        name: "掘金",
+        href: "https://juejin.im",
+        color: "is-warning"
+      },
+      {
+        name: "V2EX",
+        href: "http://www.v2ex.com",
+        color: "is-info"
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "互联网资讯",
+    items: [
+      {
+        name: "知乎",
+        href: "https://www.zhihu.com",
+        color: "is-primary"
+      },
+      {
+        name: "36氪",
+        href: "http://36kr.com",
+        color: "is-success"
+      },
+      {
+        name: "简书",
+        href: "https://www.jianshu.com",
+        color: "is-danger"
+      },
+      {
+        name: "廖雪峰",
+        href: "https://www.liaoxuefeng.com",
+        color: "is-warning"
+      },
+      {
+        name: "泡在网上的日子",
+        href: "http://www.jcodecraeer.com",
+        color: "is-info"
+      },
+      {
+        name: "阮一峰",
+        href: "http://www.ruanyifeng.com/blog",
+        color: "is-primary"
+      },
+      {
+        name: "创业邦",
+        href: "http://www.cyzone.cn",
+        color: "is-success"
+      },
+      {
+        name: "i黑马",
+        href: "http://www.iheima.com",
+        color: "is-danger"
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "公司博客",
+    items: [
+      {
+        name: "美团点评",
+        href: "https://tech.meituan.com",
+        color: "is-primary"
+      },
+      {
+        name: "悦动圈",
+        href: "https://joyrun.github.io",
+        color: "is-success"
+      },
+      {
+        name: "网易考拉",
+        href: "https://blog.klmobile.app",
+        color: "is-danger"
+      },
+      {
+        name: "有赞",
+        href: "https://tech.youzan.com",
+        color: "is-warning"
+      },
+      {
+        name: "阿里中间件",
+        href: "http://jm.taobao.org",
+        color: "is-info"
+      },
+      {
+        name: "360",
+        href: "http://blogs.360.cn",
+        color: "is-primary"
+      },
+      {
+        name: "百度移动体验部",
+        href: "http://mux.baidu.com/case",
+        color: "is-success"
+      },
+      {
+        name: "Glow",
+        href: "http://tech.glowing.com/cn",
+        color: "is-danger"
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: "代码托管",
+    items: [
+      {
+        name: "Github",
+        href: "https://github.com",
+        color: "is-primary"
+      },
+      {
+        name: "Coding",
+        href: "https://coding.net",
+        color: "is-success"
+      },
+      {
+        name: "码云",
+        href: "http://git.oschina.net",
+        color: "is-danger"
+      },
+      {
+        name: "Bitbucket",
+        href: "https://bitbucket.org",
+        color: "is-warning"
+      },
+      {
+        name: "Gitlab",
+        href: "https://about.gitlab.com",
+        color: "is-info"
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: "学习平台",
+    items: [
+      {
+        name: "慕课网",
+        href: "http://www.imooc.com",
+        color: "is-primary"
+      },
+      {
+        name: "菜鸟教程",
+        href: "http://www.runoob.com",
+        color: "is-success"
+      },
+      {
+        name: "极客学院",
+        href: "http://www.jikexueyuan.com",
+        color: "is-danger"
+      },
+      {
+        name: "网易云课堂",
+        href: "https://study.163.com",
+        color: "is-warning"
+      },
+      {
+        name: "腾讯课堂",
+        href: "https://ke.qq.com/course/list",
+        color: "is-info"
+      }
+    ]
+  },
+  {
+    id: 6,
+    title: "求职招聘",
+    items: [
+      {
+        name: "拉钩网",
+        href: "https://www.lagou.com",
+        color: "is-primary"
+      },
+      {
+        name: "Boss直聘",
+        href: "https://www.zhipin.com",
+        color: "is-success"
+      },
+      {
+        name: "前程无忧",
+        href: "https://www.51job.com",
+        color: "is-danger"
+      },
+      {
+        name: "智联招聘",
+        href: "https://www.zhaopin.com",
+        color: "is-warning"
+      },
+      {
+        name: "Leetcode",
+        href: "https://leetcode-cn.com",
+        color: "is-info"
+      },
+      {
+        name: "牛客网",
+        href: "https://www.nowcoder.com/contestRoom",
+        color: "is-primary"
+      }
+    ]
+  },
+  {
+    id: 7,
+    title: "兼职网站",
+    items: [
+      {
+        name: "程序员客栈",
+        href: "https://www.proginn.com/cat/?type=4",
+        color: "is-primary"
+      },
+      {
+        name: "码市",
+        href: "https://codemart.com",
+        color: "is-success"
+      },
+      {
+        name: "实现网",
+        href: "https://shixian.com",
+        color: "is-danger"
+      }
+    ]
+  }
+];
 export default {
   name: "home",
   data() {
@@ -99,6 +370,7 @@ export default {
         search_text: "",
         search_type: 1
       },
+      current_active_menu_id: 1,
       navs: [
         {
           id: 1,
@@ -164,7 +436,7 @@ export default {
         {
           id: 5,
           name: "githup",
-          url: "https://githup.com/search?q=",
+          url: "https://github.com/search?q=",
           is_default: false
         },
         {
@@ -181,277 +453,55 @@ export default {
           is_default: false
         }
       ],
-      navData: [
+      navData: [],
+      workData: [
         {
           id: 1,
-          title: "常用网站",
+          title: "内网快捷",
           items: [
             {
-              name: "百度",
-              href: "https://www.baidu.com",
+              name: "操盘管理后台",
+              href: "http://admin.ties.test",
               color: "is-primary"
             },
             {
-              name: "谷歌",
-              href: "https://www.google.com",
+              name: "用户管理后台",
+              href: "http://admin.tiuc.test",
               color: "is-success"
             },
             {
-              name: "必应",
-              href: "https://cn.bing.com",
+              name: "对接用户接口管理",
+              href: "http://api.tiuc.test/api",
               color: "is-danger"
             },
             {
-              name: "Stackoverflow",
-              href: "https://stackoverflow.com",
+              name: "赛事H5测试",
+              href: "http://h5.ties.t",
               color: "is-warning"
             },
             {
-              name: "Segmentfault",
-              href: "https://segmentfault.com",
-              color: "is-info"
-            },
-            {
-              name: "腾讯企业邮",
-              href:
-                "https://exmail.qq.com/cgi-bin/loginpage?t=dm_loginpage&dmtype=bizmail",
-              color: "is-primary"
-            },
-            {
-              name: "谷歌邮箱",
-              href: "https://mail.google.com/mail",
-              color: "is-success"
-            },
-            {
-              name: "Outlook邮箱",
-              href: "https://outlook.live.com/mail",
-              color: "is-danger"
-            },
-            {
-              name: "掘金",
-              href: "https://juejin.im",
-              color: "is-warning"
-            },
-            {
-              name: "V2EX",
-              href: "http://www.v2ex.com",
+              name: "赛事H5开发",
+              href: "http://h5.ties.d",
               color: "is-info"
             }
           ]
         },
         {
           id: 2,
-          title: "互联网资讯",
+          title: "内网代码",
           items: [
             {
-              name: "知乎",
-              href: "https://www.zhihu.com",
+              name: "内网gitlab代码管理",
+              href: "http://git.l",
               color: "is-primary"
-            },
-            {
-              name: "36氪",
-              href: "http://36kr.com",
-              color: "is-success"
-            },
-            {
-              name: "简书",
-              href: "https://www.jianshu.com",
-              color: "is-danger"
-            },
-            {
-              name: "廖雪峰",
-              href: "https://www.liaoxuefeng.com",
-              color: "is-warning"
-            },
-            {
-              name: "泡在网上的日子",
-              href: "http://www.jcodecraeer.com",
-              color: "is-info"
-            },
-            {
-              name: "阮一峰",
-              href: "http://www.ruanyifeng.com/blog",
-              color: "is-primary"
-            },
-            {
-              name: "创业邦",
-              href: "http://www.cyzone.cn",
-              color: "is-success"
-            },
-            {
-              name: "i黑马",
-              href: "http://www.iheima.com",
-              color: "is-danger"
-            }
-          ]
-        },
-        {
-          id: 3,
-          title: "公司博客",
-          items: [
-            {
-              name: "美团点评",
-              href: "https://tech.meituan.com",
-              color: "is-primary"
-            },
-            {
-              name: "悦动圈",
-              href: "https://joyrun.github.io",
-              color: "is-success"
-            },
-            {
-              name: "网易考拉",
-              href: "https://blog.klmobile.app",
-              color: "is-danger"
-            },
-            {
-              name: "有赞",
-              href: "https://tech.youzan.com",
-              color: "is-warning"
-            },
-            {
-              name: "阿里中间件",
-              href: "http://jm.taobao.org",
-              color: "is-info"
-            },
-            {
-              name: "360",
-              href: "http://blogs.360.cn",
-              color: "is-primary"
-            },
-            {
-              name: "百度移动体验部",
-              href: "http://mux.baidu.com/case",
-              color: "is-success"
-            },
-            {
-              name: "Glow",
-              href: "http://tech.glowing.com/cn",
-              color: "is-danger"
-            }
-          ]
-        },
-        {
-          id: 4,
-          title: "代码托管",
-          items: [
-            {
-              name: "Github",
-              href: "https://github.com",
-              color: "is-primary"
-            },
-            {
-              name: "Coding",
-              href: "https://coding.net",
-              color: "is-success"
-            },
-            {
-              name: "码云",
-              href: "http://git.oschina.net",
-              color: "is-danger"
-            },
-            {
-              name: "Bitbucket",
-              href: "https://bitbucket.org",
-              color: "is-warning"
-            },
-            {
-              name: "Gitlab",
-              href: "https://about.gitlab.com",
-              color: "is-info"
-            }
-          ]
-        },
-        {
-          id: 5,
-          title: "学习平台",
-          items: [
-            {
-              name: "慕课网",
-              href: "http://www.imooc.com",
-              color: "is-primary"
-            },
-            {
-              name: "菜鸟教程",
-              href: "http://www.runoob.com",
-              color: "is-success"
-            },
-            {
-              name: "极客学院",
-              href: "http://www.jikexueyuan.com",
-              color: "is-danger"
-            },
-            {
-              name: "网易云课堂",
-              href: "https://study.163.com",
-              color: "is-warning"
-            },
-            {
-              name: "腾讯课堂",
-              href: "https://ke.qq.com/course/list",
-              color: "is-info"
-            }
-          ]
-        },
-        {
-          id: 6,
-          title: "求职招聘",
-          items: [
-            {
-              name: "拉钩网",
-              href: "https://www.lagou.com",
-              color: "is-primary"
-            },
-            {
-              name: "Boss直聘",
-              href: "https://www.zhipin.com",
-              color: "is-success"
-            },
-            {
-              name: "前程无忧",
-              href: "https://www.51job.com",
-              color: "is-danger"
-            },
-            {
-              name: "智联招聘",
-              href: "https://www.zhaopin.com",
-              color: "is-warning"
-            },
-            {
-              name: "Leetcode",
-              href: "https://leetcode-cn.com",
-              color: "is-info"
-            },
-            {
-              name: "牛客网",
-              href: "https://www.nowcoder.com/contestRoom",
-              color: "is-primary"
-            }
-          ]
-        },
-        {
-          id: 7,
-          title: "兼职网站",
-          items: [
-            {
-              name: "程序员客栈",
-              href: "https://www.proginn.com/cat/?type=4",
-              color: "is-primary"
-            },
-            {
-              name: "大鲲",
-              href: "https://pro.lagou.com/project",
-              color: "is-success"
-            },
-            {
-              name: "实现网",
-              href: "https://shixian.com",
-              color: "is-danger"
             }
           ]
         }
       ]
     };
+  },
+  created() {
+    this.getCurrentNavs();
   },
   methods: {
     startSearch() {
@@ -469,6 +519,14 @@ export default {
       }
       let searchObj = this.searchs.find(el => el.id === id);
       window.open(`${searchObj.url}${text}`);
+    },
+    getCurrentNavs() {
+      this.navData =
+        this.current_active_menu_id === 2 ? this.workData : navsData;
+    },
+    handleChangeData(menu) {
+      this.current_active_menu_id = menu.id;
+      this.getCurrentNavs();
     }
   }
 };
