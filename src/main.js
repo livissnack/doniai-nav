8a551acd7288c300a7e171ca355dafe807f60630
guan xin <guanxin@tinytiger.cn>
2020-01-08 18:30:29 +0800

    submit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.min.css'
 
@@ -11,6 +10,5 @@ Vue.prototype.bucket_url = 'https://hspxsteel.oss-cn-shenzhen.aliyuncs.com'
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
