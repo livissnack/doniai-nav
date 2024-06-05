f2adb89007c0c2d0b0d36768688e4425791be2a3
brucesnack <brucesnack@outlook.com>
2024-06-05 16:51:44 +0800

    优化

--- src/components/tools/index.vue
+++ src/components/tools/index.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="util-list" v-if="showTools">
       <a :href="item.path" class="util-list-item" v-for="(item, index) in list" :key="index">
-        <span class="util-list-item-name">
+        <span class="util-list-item-name" :class="item.recommend ? 'is-recommend' : ''">
           <i class="util-list-item-icon fas" :class="item.icon"></i>
           {{ item.name }}
         </span>
@@ -26,34 +26,46 @@ export default {
     return {
       list: [
         {
-          name: 'JSON格式化',
+          name: '中国教育在线-掌上高考',
           icon: 'fas fa-fire-alt',
-          path: '/json'
+          path: 'https://gaokao.cn/',
+          recommend: true,
+        },
+        {
+          name: 'JSON格式化',
+          icon: 'fas fa-feather',
+          path: '/json',
+          recommend: true,
         },
         {
           name: '密码生成器',
-          icon: 'fas fa-fire-alt',
-          path: '/utils/password'
+          icon: 'fas fa-key',
+          path: '/utils/password',
+          recommend: false,
         },
         {
           name: 'TV一下',
-          icon: 'fas fa-fire-alt',
-          path: '/player'
+          icon: 'fas fa-tv',
+          path: '/player',
+          recommend: false,
         },
         {
           name: 'Base64加解密',
-          icon: 'fas fa-fire-alt',
-          path: '/utils/base64'
+          icon: 'fas fa-bold',
+          path: '/utils/base64',
+          recommend: false,
         },
         {
           name: '贷款利率计算器',
-          icon: 'fas fa-fire-alt',
-          path: '/utils/loan-rate'
+          icon: 'fas fa-money-bill',
+          path: '/utils/loan-rate',
+          recommend: false,
         },
         {
           name: '封面生成器',
-          icon: 'fas fa-fire-alt',
-          path: '/utils/cover'
+          icon: 'fas fa-images',
+          path: '/utils/cover',
+          recommend: false,
         }
       ],
       showTools: true,
@@ -121,13 +133,19 @@ export default {
       .util-list-item-name {
         font-size: 14px;
         cursor: pointer;
-        color: #D33D3D;
+        color: #000000;
         .util-list-item-icon {
-          color: #D33D3D;
+          color: #000000;
         }
         &:hover {
           color: #333333;
           text-decoration: underline;
+        }
+      }
+      .is-recommend {
+        color: #D33D3D;
+        .util-list-item-icon {
+          color: #D33D3D;
         }
       }
     }
